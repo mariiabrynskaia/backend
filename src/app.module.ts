386350b8b674fba21f17327296fc0d9181312e49
mariiabrynskaia <m.brynskaia.sas@gmail.com>
2024-02-07 10:01:28 +0300

    Database connect

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,25 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+// import { CategoryModule } from './category/category.module';
+import { getPostgresConfig } from './configs/postgres.config';
+import { PromoModule } from './promo/promo.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env',
     }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: getPostgresConfig,
+    }),
+    // CategoryModule,
+    PromoModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,25 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
 
-  const port = parseInt(process.env.PORT); // "parseint" string into number
-  // const port: number = parseInt(process.env.PORT);
+  const config = new DocumentBuilder()
+    .setTitle('Segway')
+    .setDescription(
+      `[The source API definition (json)](http://${process.env.SERVER}:${process.env.PORT}/api-json)`,
+    )
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  const port = parseInt(process.env.PORT);
+  console.log('port = ', process.env.PORT);
   const server = process.env.SERVER;
   await app.listen(port, server);
 
